3de7e7ca977cfa8575acf92f4b063eed75827878
Anton Astashov <anton.astashov@gmail.com>
2014-02-13 21:47:25 -0800

    Add 'stage' task, also from now on store SHAs for revisions

--- lib/s3_deployer.rb
+++ lib/s3_deployer.rb
@@ -31,14 +31,14 @@ class S3Deployer
 
     def deploy!
       time = Time.now.strftime(DATE_FORMAT)
-      [time, "current"].map { |dir| File.join(app_path_with_bucket, dir) }.each do |dir|
-        source_files_list.each do |file|
-          s3_file_dir = Pathname.new(File.dirname(file)).relative_path_from(Pathname.new(config.dist_dir)).to_s
-          absolute_s3_file_dir = s3_file_dir == "." ? dir : File.join(dir, s3_file_dir)
-          store_value(File.basename(file), File.read(file), absolute_s3_file_dir)
-        end
-      end
+      stage!(time)
+      copy_files_to_s3("current")
       store_current_revision(time)
+    end
+
+    def stage!(time = Time.now.strftime(DATE_FORMAT))
+      copy_files_to_s3(time)
+      store_git_hash(time)
     end
 
     def current
@@ -75,13 +75,26 @@ class S3Deployer
     def list
       puts "Getting the list of deployed revisions..."
       current_revision = get_current_revision
-      get_list_of_revisions.each do |dir|
-        string = "#{dir} - #{get_datetime_from_revision(dir)}#{" <= current" if dir == current_revision}"
+      shas_by_revisions = get_shas_by_revisions
+      get_list_of_revisions.each do |rev|
+        datetime = get_datetime_from_revision(rev)
+        sha = shas_by_revisions[rev]
+        title = sha ? `git show -s --format=%B #{sha}`.strip : nil
+        string = "#{rev} - #{datetime} #{sha ? " - #{sha[0..7]}" : ""} #{title ? "(#{title})" : ""} #{" <= current" if rev == current_revision}"
         puts string
       end
     end
 
     private
+
+      def copy_files_to_s3(rev)
+        dir = File.join(app_path_with_bucket, rev)
+        source_files_list.each do |file|
+          s3_file_dir = Pathname.new(File.dirname(file)).relative_path_from(Pathname.new(config.dist_dir)).to_s
+          absolute_s3_file_dir = s3_file_dir == "." ? dir : File.join(dir, s3_file_dir)
+          store_value(File.basename(file), File.read(file), absolute_s3_file_dir)
+        end
+      end
 
       def get_list_of_revisions
         prefix = File.join(config.app_path)
@@ -101,6 +114,16 @@ class S3Deployer
         date.strftime("%m/%d/%Y %H:%M") if date
       end
 
+      def get_shas_by_revisions
+        get_value("SHAS", app_path_with_bucket).split("\n").inject({}) do |memo, line|
+          revision, sha = line.split(" - ").map(&:strip)
+          memo[revision] = sha
+          memo
+        end
+      rescue AWS::S3::NoSuchKey
+        {}
+      end
+
       def current_revision_path
         File.join(app_path_with_bucket, CURRENT_REVISION)
       end
@@ -112,6 +135,13 @@ class S3Deployer
 
       def store_current_revision(revision)
         store_value(File.basename(current_revision_path), revision, File.dirname(current_revision_path))
+      end
+
+      def store_git_hash(time)
+        shas_by_revisions = get_shas_by_revisions
+        shas_by_revisions[time] = `git rev-parse HEAD`.strip
+        value = shas_by_revisions.map { |sha, rev| "#{sha} - #{rev}" }.join("\n")
+        store_value("SHAS", value, app_path_with_bucket)
       end
 
       def get_current_revision
