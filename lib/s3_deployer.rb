53120d7e83203d14b7300aeac1f7e3e7ef5eb25e
Anton Astashov <anton.astashov@gmail.com>
2015-12-14 22:50:31 -0600

    Fix failing in Ruby 2.2.3
    By some reason it worked fine in 2.1.2, but not in 2.2.3
    It seems like importing of aws-sdk is messed up, so we need
    to help it a bit by importing EndpointProvider manually

--- lib/s3_deployer.rb
+++ lib/s3_deployer.rb
@@ -5,6 +5,7 @@ require 'tzinfo'
 require 'rexml/document'
 require 'parallel'
 require 'aws-sdk'
+require 'aws-sdk-core/endpoint_provider' # it's needed by some reason for Ruby 2.2.3
 
 require "s3_deployer/config"
 require "s3_deployer/color"
